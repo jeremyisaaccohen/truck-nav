15969dd2beb092a1a1136194eeb0308ec15f8325
Jeremy Cohen <jeremycohen2323@gmail.com>
2024-07-16 14:16:55 -0400

    Initial setup, can submit from and to addresses

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # truck-nav
 GPS or static route generation for commercial vehicles
+
+### Setup
+
+poetry install --no-root     
+
+`cd Downloads/kafka/kafka_2.13-3.7.0/`
+
+Start the ZooKeeper service 
+
+`$ bin/zookeeper-server-start.sh config/zookeeper.properties`
+
+And in another terminal, start the Kafka broker service:
+
+`$ bin/kafka-server-start.sh config/server.properties`
+
+`fastapi run [FILE].py --reload`
+
+reload flag useful for live reloads of changes in src
---
+++ frontend.py
@@ -0,0 +1,53 @@
+"""FastAPI to accept post requests and publish to kafka"""
+import json
+
+from kafka import KafkaProducer
+from kafka.admin import NewTopic
+
+from fastapi import FastAPI, Form
+from fastapi import Request
+
+from fastapi.templating import Jinja2Templates
+
+from constants import NUM_PARTITIONS, REPLICATION_FACTOR, BOOTSTRAP_SERVERS, TRAFFIC_TOPIC_NAME
+
+traffic_topic = NewTopic(TRAFFIC_TOPIC_NAME, num_partitions=NUM_PARTITIONS, replication_factor=REPLICATION_FACTOR)
+
+producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVERS])
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates/")
+
+# producer.send(topic=my_topic.name, value=sample_data)
+print("sent")
+
+@app.get('/')
+async def root(request: Request):
+    return templates.TemplateResponse('index.html', context={'request':request})
+
+
+@app.post('/')
+async def post(request: Request, from_address: str = Form(...), to_address: str = Form(...)):
+    print(f"jib request: {request}\njib from{ from_address} to: {to_address}")
+    try:
+        data = {"from_address": from_address, "to_address":to_address}
+        print(data)
+        dumped = json.dumps(data)
+        encoded = dumped.encode('utf-8')
+        print("Encoded data:", data)
+
+        # Assuming producer is set up correctly, this is the Kafka sending part
+        producer.send(traffic_topic.name, value=encoded)
+        producer.flush()
+
+        return templates.TemplateResponse('index.html', context={'request': request, 'result': f'HERE: {dumped}'})
+    except Exception as e:
+        print("Error occurred:", e)
+        return templates.TemplateResponse('index.html', context={'request': request, 'result': f'Error: {str(e)}'})
+
+
+
+
+print(producer.metrics())
+
